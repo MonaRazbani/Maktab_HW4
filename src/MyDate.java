392b656957236e9d9d49c5120953ef2cb5d5fb81
mona <mona.razbani1@gmail.com git config --global user.email mona.razbani1@gmail.com>
2021-09-21 00:53:21 +0430

    add toString and next and getter methods for MyDate class

--- src/MyDate.java
+++ src/MyDate.java
@@ -6,7 +6,7 @@
         this.month = month;
         this.year = year;
     }
-<<<<<<< HEAD
+
     
            public boolean isValidDate(int day, int month, int year) {
                if (year > 0 && year < 9999) {
@@ -35,38 +35,52 @@
                else
                    return -1;
            }
+       Month months[] = Month.values();
 
-
-       }
-=======
-       public boolean isValidDate(int day, int month, int year) {
-           if (year > 0 && year < 9999) {
-               if (month >= 1 && month <= 6) {
-                   if (day >= 1 && day <= 31)
-                       return true;
-               } else if (month >= 7 && month <= 11) {
-                   if (day >= 1 && day <= 30)
-                       return true;
-               } else if (month == 12) {
-                   if (day >= 1 && day <= 29)
-                       return true;
-               }
-
+       public String toString() {
+           for (Month item : months) {
+               if (item.getAbbr() == getMonth())
+                   return getDay() + " " + item + " " + getYear();
            }
-           return false;
+           return null;
+       }
+       public void next (){
+           if (getMonthLastDay(getMonth())!=getDay()){
+               setDay(getDay()+1);
+           }
+           else {
+               if (getMonthLastDay(getMonth())==getDay()&& getMonth()!=12 ){
+                   setDay(1);
+                   setMonth(getMonth()+1);
+               }
+               else {
+                   setMonth(1);
+                   setDay(1);
+                   setYear(getYear()+1);
+               }
+           }
+       }
+       public int getDay() {
+           return day;
        }
 
-       public int getMonthLastDay(int month) {
-           if (month >= 0 && month <= 6)
-               return 31;
-           else if (month >= 7 && month <= 11)
-               return 30;
-           else if (month == 12)
-               return 29;
-           else
-               return -1;
+       public int getMonth() {
+           return month;
        }
 
-   }
->>>>>>> 9860ddd4b0d973773f54847576faebf4a9dd55a4
+       public int getYear() {
+           return year;
+       }
 
+       public void setDay(int day) {
+           this.day = day;
+       }
+
+       public void setMonth(int month) {
+           this.month = month;
+       }
+
+       public void setYear(int year) {
+           this.year = year;
+       }
+       }
