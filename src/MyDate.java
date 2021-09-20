1264d5eb5fb4fc30139ea6d1ac66aed02a1b4978
mona <mona.razbani1@gmail.com git config --global user.email mona.razbani1@gmail.com>
2021-09-20 23:31:11 +0430

    creat MyDate constractor

---
+++ src/MyDate.java
@@ -0,0 +1,10 @@
+   public class MyDate {
+    private int day, month, year;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
+
