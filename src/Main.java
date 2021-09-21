e508330bb0f0ce7ea39d1e5d68e2a15f27a07478
mona <mona.razbani1@gmail.com git config --global user.email mona.razbani1@gmail.com>
2021-09-21 17:33:14 +0430

    compelete main

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
+
 import java.util.Scanner;
+                    public class Main {
+                        public static void main(String[] args) {
+                            Scanner scanner = new Scanner(System.in);
+                            while (true) {
+                                System.out.println("Enter a date\nEnter day :");
+                                int day = scanner.nextInt();
+                                System.out.println("Enter month:");
+                                int month = scanner.nextInt();
+                                System.out.println("Enter year:");
+                                int year = scanner.nextInt();
+                                MyDate myDate = new MyDate(day, month, year);
+                                if (myDate.isValidDate(day, month, year)) {
+                                    System.out.println("your date is valid");
+                                    System.out.println(myDate.toString());
+                                } else
+                                    System.out.println("your date is not valid");
+                                myDate.getMonthLastDay(month);
+                            myDate.next();
+                                System.out.println(myDate);
+                            }
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter a date\nEnter day :");
-            int day = scanner.nextInt();
-            System.out.println("Enter month:");
-            int month = scanner.nextInt();
-            System.out.println("Enter year:");
-            int year = scanner.nextInt();
-            MyDate myDat = new MyDate(day, month, year);
-            if (myDat.isValidDate(day, month, year)) {
-                System.out.println("your date is valid");
-                System.out.println(myDat.toString());
-            } else
-                System.out.println("your date is not valid !");
-            myDat.getMonthLastDay(month);
-
-
-        }
-    }
-}
+                        }
+                    }
